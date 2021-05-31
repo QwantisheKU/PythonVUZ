2c52ca6e9d61a063a94dec01832f3920a1f15af4
qwantisheku <odargov@gmail.com>
2021-05-31 19:27:27 +0300

    3, 4, 5, 6 extra

---
+++ extra3/main.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="root",
+    version="1.0.0",
+    author="OLEG",
+    author_email="odargov@gmail.com",
+    packages=["root"],
+    description="Some important information"
+)
